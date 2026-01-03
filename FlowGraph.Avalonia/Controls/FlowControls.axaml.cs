0e01f0113aadb9cba5d9fba9481b5c9206d6ee65
Takeshi <1505448+iwatakeshi@users.noreply.github.com>
2026-01-02 23:00:53 -0700

    feat: Add debug toggle button to show/hide diagnostics panel

--- FlowGraph.Avalonia/Controls/FlowControls.axaml.cs
+++ FlowGraph.Avalonia/Controls/FlowControls.axaml.cs
@@ -10,10 +10,22 @@ public partial class FlowControls : UserControl
     public static readonly StyledProperty<FlowCanvas?> TargetCanvasProperty =
         AvaloniaProperty.Register<FlowControls, FlowCanvas?>(nameof(TargetCanvas));
 
+    public static readonly StyledProperty<FlowDiagnostics?> DiagnosticsPanelProperty =
+        AvaloniaProperty.Register<FlowControls, FlowDiagnostics?>(nameof(DiagnosticsPanel));
+
     public FlowCanvas? TargetCanvas
     {
         get => GetValue(TargetCanvasProperty);
         set => SetValue(TargetCanvasProperty, value);
+    }
+
+    /// <summary>
+    /// The diagnostics panel to show/hide with the debug toggle button.
+    /// </summary>
+    public FlowDiagnostics? DiagnosticsPanel
+    {
+        get => GetValue(DiagnosticsPanelProperty);
+        set => SetValue(DiagnosticsPanelProperty, value);
     }
 
     private Button? _zoomInButton;
@@ -21,6 +33,7 @@ public partial class FlowControls : UserControl
     private Button? _fitViewButton;
     private Button? _centerButton;
     private Button? _resetZoomButton;
+    private Button? _debugToggleButton;
 
     public FlowControls()
     {
@@ -36,6 +49,7 @@ public partial class FlowControls : UserControl
         _fitViewButton = this.FindControl<Button>("FitViewButton");
         _centerButton = this.FindControl<Button>("CenterButton");
         _resetZoomButton = this.FindControl<Button>("ResetZoomButton");
+        _debugToggleButton = this.FindControl<Button>("DebugToggleButton");
 
         if (_zoomInButton != null)
             _zoomInButton.Click += OnZoomInClick;
@@ -47,6 +61,8 @@ public partial class FlowControls : UserControl
             _centerButton.Click += OnCenterClick;
         if (_resetZoomButton != null)
             _resetZoomButton.Click += OnResetZoomClick;
+        if (_debugToggleButton != null)
+            _debugToggleButton.Click += OnDebugToggleClick;
     }
 
     private void OnZoomInClick(object? sender, RoutedEventArgs e)
@@ -72,5 +88,13 @@ public partial class FlowControls : UserControl
     private void OnResetZoomClick(object? sender, RoutedEventArgs e)
     {
         TargetCanvas?.ResetZoom();
+    }
+
+    private void OnDebugToggleClick(object? sender, RoutedEventArgs e)
+    {
+        if (DiagnosticsPanel != null)
+        {
+            DiagnosticsPanel.IsVisible = !DiagnosticsPanel.IsVisible;
+        }
     }
 }
