f04d6780150b622fd0a15e032be390b814efa3ea
Takeshi <tak@prismify.co>
2026-01-15 12:53:26 -0700

    refactor: extract EdgeVisualManager label methods to Labels.cs partial

--- FlowGraph.Avalonia/Rendering/DirectGraphRenderer.DrawingHelpers.cs
+++ FlowGraph.Avalonia/Rendering/DirectGraphRenderer.DrawingHelpers.cs
@@ -9,41 +9,41 @@ namespace FlowGraph.Avalonia.Rendering;
 
 public partial class DirectGraphRenderer
 {
-    #region Drawing Helpers
+  #region Drawing Helpers
 
-    private static StreamGeometry CreateRoundedRectGeometry(Rect rect, double radius)
+  private static StreamGeometry CreateRoundedRectGeometry(Rect rect, double radius)
+  {
+    var geometry = new StreamGeometry();
+    using (var ctx = geometry.Open())
     {
-        var geometry = new StreamGeometry();
-        using (var ctx = geometry.Open())
-        {
-            ctx.BeginFigure(new AvaloniaPoint(rect.Left + radius, rect.Top), true);
-            ctx.LineTo(new AvaloniaPoint(rect.Right - radius, rect.Top));
-            ctx.ArcTo(new AvaloniaPoint(rect.Right, rect.Top + radius), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
-            ctx.LineTo(new AvaloniaPoint(rect.Right, rect.Bottom - radius));
-            ctx.ArcTo(new AvaloniaPoint(rect.Right - radius, rect.Bottom), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
-            ctx.LineTo(new AvaloniaPoint(rect.Left + radius, rect.Bottom));
-            ctx.ArcTo(new AvaloniaPoint(rect.Left, rect.Bottom - radius), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
-            ctx.LineTo(new AvaloniaPoint(rect.Left, rect.Top + radius));
-            ctx.ArcTo(new AvaloniaPoint(rect.Left + radius, rect.Top), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
-            ctx.EndFigure(true);
-        }
-        return geometry;
+      ctx.BeginFigure(new AvaloniaPoint(rect.Left + radius, rect.Top), true);
+      ctx.LineTo(new AvaloniaPoint(rect.Right - radius, rect.Top));
+      ctx.ArcTo(new AvaloniaPoint(rect.Right, rect.Top + radius), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
+      ctx.LineTo(new AvaloniaPoint(rect.Right, rect.Bottom - radius));
+      ctx.ArcTo(new AvaloniaPoint(rect.Right - radius, rect.Bottom), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
+      ctx.LineTo(new AvaloniaPoint(rect.Left + radius, rect.Bottom));
+      ctx.ArcTo(new AvaloniaPoint(rect.Left, rect.Bottom - radius), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
+      ctx.LineTo(new AvaloniaPoint(rect.Left, rect.Top + radius));
+      ctx.ArcTo(new AvaloniaPoint(rect.Left + radius, rect.Top), new Size(radius, radius), 0, false, SweepDirection.Clockwise);
+      ctx.EndFigure(true);
     }
+    return geometry;
+  }
 
-    private void DrawCenteredText(DrawingContext context, string text, Rect bounds, double fontSize, IBrush brush)
-    {
-        var formattedText = new FormattedText(
-            text,
-            System.Globalization.CultureInfo.CurrentCulture,
-            FlowDirection.LeftToRight,
-            _typeface,
-            fontSize,
-            brush);
+  private void DrawCenteredText(DrawingContext context, string text, Rect bounds, double fontSize, IBrush brush)
+  {
+    var formattedText = new FormattedText(
+        text,
+        System.Globalization.CultureInfo.CurrentCulture,
+        FlowDirection.LeftToRight,
+        _typeface,
+        fontSize,
+        brush);
 
-        var textX = bounds.X + (bounds.Width - formattedText.Width) / 2;
-        var textY = bounds.Y + (bounds.Height - formattedText.Height) / 2;
-        context.DrawText(formattedText, new AvaloniaPoint(textX, textY));
-    }
+    var textX = bounds.X + (bounds.Width - formattedText.Width) / 2;
+    var textY = bounds.Y + (bounds.Height - formattedText.Height) / 2;
+    context.DrawText(formattedText, new AvaloniaPoint(textX, textY));
+  }
 
-    #endregion
+  #endregion
 }
--- FlowGraph.Avalonia/Rendering/DirectGraphRenderer.InlineEditing.cs
+++ FlowGraph.Avalonia/Rendering/DirectGraphRenderer.InlineEditing.cs
@@ -5,45 +5,45 @@ namespace FlowGraph.Avalonia.Rendering;
 
 public partial class DirectGraphRenderer
 {
-    #region Inline Editing Support
+  #region Inline Editing Support
 
-    /// <summary>
-    /// Begins editing a node's label. The label will not be drawn while editing.
-    /// </summary>
-    /// <param name="nodeId">ID of the node to edit.</param>
-    public void BeginEditNode(string nodeId)
-    {
-        _editingNodeId = nodeId;
-        InvalidateVisual();
-    }
+  /// <summary>
+  /// Begins editing a node's label. The label will not be drawn while editing.
+  /// </summary>
+  /// <param name="nodeId">ID of the node to edit.</param>
+  public void BeginEditNode(string nodeId)
+  {
+    _editingNodeId = nodeId;
+    InvalidateVisual();
+  }
 
-    /// <summary>
-    /// Ends editing a node's label.
-    /// </summary>
-    public void EndEditNode()
-    {
-        _editingNodeId = null;
-        InvalidateVisual();
-    }
+  /// <summary>
+  /// Ends editing a node's label.
+  /// </summary>
+  public void EndEditNode()
+  {
+    _editingNodeId = null;
+    InvalidateVisual();
+  }
 
-    /// <summary>
-    /// Begins editing an edge's label. The label will not be drawn while editing.
-    /// </summary>
-    /// <param name="edgeId">ID of the edge to edit.</param>
-    public void BeginEditEdge(string edgeId)
-    {
-        _editingEdgeId = edgeId;
-        InvalidateVisual();
-    }
+  /// <summary>
+  /// Begins editing an edge's label. The label will not be drawn while editing.
+  /// </summary>
+  /// <param name="edgeId">ID of the edge to edit.</param>
+  public void BeginEditEdge(string edgeId)
+  {
+    _editingEdgeId = edgeId;
+    InvalidateVisual();
+  }
 
-    /// <summary>
-    /// Ends editing an edge's label.
-    /// </summary>
-    public void EndEditEdge()
-    {
-        _editingEdgeId = null;
-        InvalidateVisual();
-    }
+  /// <summary>
+  /// Ends editing an edge's label.
+  /// </summary>
+  public void EndEditEdge()
+  {
+    _editingEdgeId = null;
+    InvalidateVisual();
+  }
 
-    #endregion
+  #endregion
 }
